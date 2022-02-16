f46a6d5043bec4ac3ef7994c0f09e2cd1c26c91a
Gabriel Costa <gabrielcostalavor0@gmail.com>
2022-02-16 16:38:08 -0300

    layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,29 @@
 import type { AppProps } from 'next/app'
-import {ChakraProvider} from '@chakra-ui/react'
+import {ChakraProvider, Container, Grid, GridItem} from '@chakra-ui/react'
 import { theme } from '../styles/theme'
+import { Header } from '../components/header'
+import { Footer } from '../components/footer'
+import { Aside } from '../components/aside'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {/**ChakraProvider serve para habilitar o chakra, esse theme é pra por o a variavel com estilização pro thema, resetCSS é pra resetar o css */}
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+        <Grid h='100vh' templateRows='auto 1fr auto' templateColumns='auto'>
+          <GridItem colSpan={1} rowSpan={1}>
+            <Header/>  
+          </GridItem>
+          <GridItem colSpan={1} rowSpan={1}>
+            <Container maxW='container.xl' display='flex'>
+              <Aside/>
+              <Component {...pageProps} />
+            </Container>
+          </GridItem>
+          <GridItem colSpan={1} rowSpan={1}>
+            <Footer/>
+          </GridItem>
+        </Grid>
       </ChakraProvider>
     </>
     )
